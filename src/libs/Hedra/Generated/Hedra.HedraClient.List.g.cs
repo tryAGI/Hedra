d64a5da90ea70c0ca9768a1cff703d9d7a292e08
Konstantin S. <havendv@gmail.com>
2026-05-24 08:11:25 +0400

    feat: Updated OpenAPI spec (#144)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Hedra/Generated/Hedra.HedraClient.List.g.cs
+++ src/libs/Hedra/Generated/Hedra.HedraClient.List.g.cs
@@ -430,18 +430,17 @@ namespace Hedra
                                     __exception_422 = __ex;
                                 }
 
-                                throw new global::Hedra.ApiException<global::Hedra.HTTPValidationError>(
+
+                                throw global::Hedra.ApiException<global::Hedra.HTTPValidationError>.Create(
+                                    statusCode: __response.StatusCode,
                                     message: __content_422 ?? __response.ReasonPhrase ?? string.Empty,
                                     innerException: __exception_422,
-                                    statusCode: __response.StatusCode)
-                                {
-                                    ResponseBody = __content_422,
-                                    ResponseObject = __value_422,
-                                    ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                    responseBody: __content_422,
+                                    responseObject: __value_422,
+                                    responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                         __response.Headers,
                                         h => h.Key,
-                                        h => h.Value),
-                                };
+                                        h => h.Value));
                             }
 
                             if (__effectiveReadResponseAsString)
@@ -475,17 +474,15 @@ namespace Hedra
                                 }
                                 catch (global::System.Exception __ex)
                                 {
-                                    throw new global::Hedra.ApiException(
+                                    throw global::Hedra.ApiException.Create(
+                                        statusCode: __response.StatusCode,
                                         message: __content ?? __response.ReasonPhrase ?? string.Empty,
                                         innerException: __ex,
-                                        statusCode: __response.StatusCode)
-                                    {
-                                        ResponseBody = __content,
-                                        ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                        responseBody: __content,
+                                        responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                             __response.Headers,
                                             h => h.Key,
-                                            h => h.Value),
-                                    };
+                                            h => h.Value));
                                 }
                             }
                             else
@@ -522,17 +519,15 @@ namespace Hedra
                                     {
                                     }
 
-                                    throw new global::Hedra.ApiException(
+                                    throw global::Hedra.ApiException.Create(
+                                        statusCode: __response.StatusCode,
                                         message: __content ?? __response.ReasonPhrase ?? string.Empty,
                                         innerException: __ex,
-                                        statusCode: __response.StatusCode)
-                                    {
-                                        ResponseBody = __content,
-                                        ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                        responseBody: __content,
+                                        responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                             __response.Headers,
                                             h => h.Key,
-                                            h => h.Value),
-                                    };
+                                            h => h.Value));
                                 }
                             }
 
